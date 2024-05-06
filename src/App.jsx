4fac88009d94e81c63ad06b1c935b1b1c9e653d3
AtticaWebDev <agboton.dev@proton.me>
2024-05-06 05:40:10 +0200

    Morpix Game

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Morpix } from "./components/Morpix/Morpix";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Morpix />
+      </div>
+    </>
+  );
+}
+
+export default App;
